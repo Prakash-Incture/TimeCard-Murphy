4e6fa56fe83a41fc47b99235eddfeff187d39584
Prakash <prakash.anumolu@incture.com>
2020-01-09 08:19:22 +0530

    Small changes

--- TimeCard/Absences/Views/Custom Cells/TableViewCells/CalenderTableViewCell.swift
+++ TimeCard/Absences/Views/Custom Cells/TableViewCells/CalenderTableViewCell.swift
@@ -13,17 +13,33 @@ class CalenderTableViewCell: UITableViewCell {
     @IBOutlet weak var calenderView: FSCalendar!
     @IBOutlet weak var leftButtonAction: UIButton!
     @IBOutlet weak var rightAction: UIButton!
+    @IBOutlet weak var datelabel: UILabel!
+    
     var panGesture = UIPanGestureRecognizer(target: self, action:(Selector(("handlePanGesture:"))))
-
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMM yyyy"
+        formatter.timeZone = TimeZone(secondsFromGMT:Int(5.30))
+        return formatter
+    }()
+    var previousDate:String?
+    var today:Date?
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.calenderSetUP()
+    }
+    func calenderSetUP(){
         calenderView.scope = .week
+        calenderView.firstWeekday = 2
         calenderView.appearance.headerMinimumDissolvedAlpha = 0.0
-        calenderView.appearance.headerDateFormat = "MMM yyyy"
         calenderView.calendarHeaderView.isHidden = true
         calenderView.delegate = self
+        calenderView.dataSource = self
         calenderView.addGestureRecognizer(panGesture)
+        self.previousDate = self.dateFormatter.string(from: self.calenderView.currentPage)
+        today = Date().startOfWeek
+        self.datelabel.text = "\(self.dateFormatter.string(from:self.getCurrentWeekDays()[0])) - \(self.dateFormatter.string(from:self.getCurrentWeekDays()[self.getCurrentWeekDays().count - 1]))"
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -31,24 +47,44 @@ class CalenderTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     @IBAction func leftButtonAction(_ sender: Any) {
-        self.calenderView.currentPage = .distantFuture
-        self.calenderView.reloadData()
+        let currentDate = self.calenderView.currentPage
+        let prevData = Calendar.current.date(byAdding: .day, value: 1, to: currentDate.startOfWeek!)
+        self.calenderView.setCurrentPage(prevData ?? Date(), animated: true)
+
     }
     @IBAction func rightButtonAction(_ sender: Any) {
-        self.calenderView.currentPage = .distantPast
-        self.calenderView.reloadData()
-
+        let currentDate = self.calenderView.currentPage
+        let prevData = Calendar.current.date(byAdding: .day, value: 1, to: currentDate)
+        self.calenderView.setCurrentPage(prevData ?? Date(), animated: true)
     }
     func handlePanGesture(panGesture: UIPanGestureRecognizer) {
 
     }
 }
-extension CalenderTableViewCell:FSCalendarDelegate{
+extension CalenderTableViewCell:FSCalendarDelegate,FSCalendarDataSource{
     
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        print(date)
         NotificationCenter.default.post(name: Notification.Name(rawValue: "onTapOfDate"), object:date)
     }
     func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        today = self.getCurrentWeekDays()[self.getCurrentWeekDays().count - 1]
+        self.datelabel.text = "\(self.dateFormatter.string(from:self.getCurrentWeekDays()[0])) - \(self.dateFormatter.string(from:self.getCurrentWeekDays()[self.getCurrentWeekDays().count - 1]))"
     }
+    func getCurrentWeekDays() -> [Date]{
+        var days:[Date] = []
+        for dateIndex in 0..<7 {
+            guard let startWeek = today else { return [Date()]}
+                   let date = Calendar.current.date(byAdding: .day, value: dateIndex + 1, to: startWeek)
+                      days.append(date ?? Date())
+               }
+           return days
+    }
+}
+extension Date {
+    var startOfWeek: Date? {
+        let gregorian = Calendar(identifier: .gregorian)
+        guard let sunday = gregorian.date(from: gregorian.dateComponents([.weekOfYear, .weekOfYear], from: self)) else { return nil }
+        return gregorian.date(byAdding: .day, value: 1, to: sunday)
+    }
+    
 }
